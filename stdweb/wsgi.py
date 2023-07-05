402c8621bc4c51b218242a9b8089a505c34058e9
karpov-sv <karpov.sv@gmail.com>
2023-07-05 19:53:03 +0200

    Minor adjustments for deployment through mod_wsgi

--- stdweb/wsgi.py
+++ stdweb/wsgi.py
@@ -7,9 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+import locale
 
 from django.core.wsgi import get_wsgi_application
+
+this_path = os.path.split(os.path.abspath(__file__))[0]
+this_uppath = os.path.abspath(os.path.join(this_path, '..'))
+
+sys.path.append(this_uppath)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stdweb.settings')
 
