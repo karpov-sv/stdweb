e677167167539d2d295d0dfe324c4cc807fbc18e
karpov.sv@gmail.com <karpov.sv@gmail.com>
2023-07-10 02:11:15 +0200

    Ignore hidden files

--- run_celery.sh
+++ run_celery.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-watchmedo auto-restart -d stdweb -p '*.py' -- celery -A stdweb worker --loglevel=info
+watchmedo auto-restart -d stdweb -p '*.py' --ignore-patterns="*/.*" -- celery -A stdweb worker --loglevel=info
