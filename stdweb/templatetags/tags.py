ff27ea4f46d6ef226ddd7d39cd40478cdce345f5
karpov.sv@gmail.com <karpov.sv@gmail.com>
2023-07-14 23:08:19 +0200

    Fix displaying file: markup with arbitrary extension in log files

--- stdweb/templatetags/tags.py
+++ stdweb/templatetags/tags.py
@@ -48,7 +48,7 @@ def task_file_contents(task, filename, highlight=False):
                           r"\1<span class='text-danger'>\2</span>",
                           contents, flags=re.MULTILINE)
 
-        contents = re.sub(r"\b(file:(\w+\.(fits|vot|txt|cutout)))\b",
+        contents = re.sub(r"\b(file:(\w+\.\w+))\b",
                           partial(task_file_link, task=task),
                           contents, flags=re.MULTILINE)
 
