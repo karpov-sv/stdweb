3d657d62f375c74e0e4b49c3f168bcd18857ce7d
Sergey Karpov <karpov.sv@gmail.com>
2024-03-25 23:44:10 +0100

    Avoid crashing for non-authenticated users

--- stdweb/views_tasks.py
+++ stdweb/views_tasks.py
@@ -174,7 +174,9 @@ def tasks(request, id=None):
             if form.is_valid():
                 show_all = form.cleaned_data.get('show_all')
                 if not show_all:
-                    tasks = tasks.filter(user = request.user)
+                    if request.user.is_authenticated:
+                        tasks = tasks.filter(user = request.user)
+
                 query = form.cleaned_data.get('query')
                 if query:
                     for token in query.split():
