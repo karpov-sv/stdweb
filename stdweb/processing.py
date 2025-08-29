4e82ce9475622a52417140ebc0808672318d3701
karpov-sv <karpov.sv@gmail.com>
2025-08-29 21:02:01 +0200

    Adjust template fwhm mag plot name to better reflect what it is

--- stdweb/processing.py
+++ stdweb/processing.py
@@ -2176,7 +2176,7 @@ def subtract_image(filename, config, verbose=True, show=False):
             template_fwhm = config.get('template_fwhm_override')
 
         # Plot FWHM vs instrumental. TODO: make generic function for that?..
-        with plots.figure_saver(os.path.join(basepath, 'sub_fwhm_mag.png'), figsize=(8, 6), show=show) as fig:
+        with plots.figure_saver(os.path.join(basepath, 'sub_template_fwhm_mag.png'), figsize=(8, 6), show=show) as fig:
             fwhm_values = 2.0*tobj['FLUX_RADIUS'] # obj['fwhm']
             ax = fig.add_subplot(1, 1, 1)
             ax.plot(fwhm_values, tobj['mag'], '.', label='All objects')
