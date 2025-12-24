a03c3743bd9402ca7ab66dbe4b25d7036b2060c7
karpov-sv <karpov.sv@gmail.com>
2025-12-25 00:38:29 +0100

    Minor local updates

--- run_celery.sh
+++ run_celery.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Avoid crashing on newer MacOS
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
 watchmedo auto-restart -d stdweb -p '*.py' --ignore-patterns="*/.*" -- python3 -m celery -- -A stdweb worker --loglevel=info
